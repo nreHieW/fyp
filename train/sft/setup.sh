7aa2ec7554334336b375f7509a2f824976479dcb
nreHieW <limweihern@gmail.com>
2025-09-11 10:45:23 +0800

    add setup.sh

--- train/sft/setup.sh
+++ train/sft/setup.sh
@@ -1,24 +1,27 @@
 sudo apt-get update
+python -m pip install --upgrade pip wheel setuptools
+
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source ~/.bashrc
 export PATH="/root/.cargo/bin:$PATH"
 
-python -m pip install --upgrade pip wheel setuptools
 
 git clone --depth 1 https://github.com/hiyouga/LLaMA-Factory.git
 cd LLaMA-Factory
 uv sync --extra torch --extra metrics --prerelease=allow
-uv pip install -e ".[torch,metrics]" --no-build-isolation
-uv pip install deepspeed==0.14.4
-uv pip install --no-build-isolation flash-attn==2.7.3
-uv pip install datasets 
-uv pip install vllm
-uv pip install ray
+source .venv/bin/activate
+# uv pip install -e ".[torch,metrics]" --no-build-isolation --prerelease=allow
+uv add wheel
+uv add deepspeed==0.14.4
+uv add --no-build-isolation flash-attn==2.7.3
+uv add datasets 
+uv add vllm
+uv add ray
 
 cd ../
 rm -rf LLaMA-Factory/data/
 
-python prepare_dataset.py 
+uv run prepare_dataset.py 
 mv ../configs/config.yaml LLaMA-Factory/
 
 uv pip install wandb
@@ -29,5 +32,4 @@ export WANDB_PROJECT="partial-edits"
 uv run huggingface-cli login
 
 cd LLaMA-Factory
-# uv run llamafactory-cli train config.yaml
 # uv run --prerelease=allow llamafactory-cli train config.yaml
