f5532133eec4da6ad589cb46da1d566d8233e12f
nreHieW <limweihern@gmail.com>
2025-11-11 19:35:28 +0800

    update

--- train/sft/setup.sh
+++ train/sft/setup.sh
@@ -1,21 +1,21 @@
-# sudo apt-get update
-# python -m pip install --upgrade pip wheel setuptools
+sudo apt-get update
+python -m pip install --upgrade pip wheel setuptools
 
-# curl -LsSf https://astral.sh/uv/install.sh | sh
-# source ~/.bashrc
-# export PATH="/root/.cargo/bin:$PATH"
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source ~/.bashrc
+export PATH="/root/.cargo/bin:$PATH"
 
 
-# git clone --depth 1 https://github.com/hiyouga/LLaMA-Factory.git
+git clone --depth 1 https://github.com/hiyouga/LLaMA-Factory.git
 cd LLaMA-Factory
 uv sync --extra torch --extra metrics --prerelease=allow
 source .venv/bin/activate
-# uv pip install -e ".[torch,metrics]" --no-build-isolation --prerelease=allow
+uv pip install -e ".[torch,metrics]" --no-build-isolation --prerelease=allow
 uv pip install wheel
 uv pip install deepspeed==0.14.4
 uv pip install --no-build-isolation flash-attn==2.7.3
-# uv pip install datasets 
-# uv add vllm
+uv pip install datasets 
+uv add vllm
 uv pip install ray
 
 cd ../
